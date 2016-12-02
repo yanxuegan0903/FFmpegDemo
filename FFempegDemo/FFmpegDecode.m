569f050966fa012ebe9970b623d9a7f70dea2f00
yanxuegan <yanxuegan0903@163.com>
2016-12-02 10:22:00 +0800

    解码替换为老的接口  解码正常

--- FFempegDemo/FFmpegDecode.m
+++ FFempegDemo/FFmpegDecode.m
@@ -28,6 +28,11 @@
 {
     AAPLEAGLLayer * _playerLayer;
     NSLock * _dstDataLock;
+    
+    
+    
+    
+    
 }
 
 
@@ -87,7 +92,6 @@
         }
     }
     
-    
     int version = avcodec_version();
     NSLog(@"version = %d",version);
     return (BOOL)self.frame;
@@ -105,46 +109,51 @@ static int count = 0;
     static int i;
     i++;
     count++;
+        
+    _packet.data = (uint8_t *)VideoData.bytes;
+    _packet.size = (int)VideoData.length;
 
-//    @autoreleasepol {
+    
+    int got_picture_ptr;
+    int status = avcodec_decode_video2(self.codecCtx, self.frame, &got_picture_ptr, &(_packet));
+    if (status >0)
+    {
+        [self dispatchAVFrame:self.frame];
+    }else
+    {
+        avcodec_flush_buffers(self.codecCtx);
+    }
+
+    
+    //    int sendDecodeStatu,receiveDecodeStatus;
+//    if (i <= 1000000){
+//        sendDecodeStatu = avcodec_send_packet(_codecCtx, &_packet);
+//        if (sendDecodeStatu == 0)
+//        {
+//            NSLog(@"sendPacket success");
+//            receiveDecodeStatus = avcodec_receive_frame(self.codecCtx, self.frame);
+//            if (receiveDecodeStatus == 0)
+//            {
+//                NSLog(@"receive Packet success");
+//                [self dispatchAVFrame:self.frame];
+//                
+//            }else if(receiveDecodeStatus == AVERROR_EOF){
+//                avcodec_flush_buffers(_codecCtx);
+//                NSLog(@"receiveDecodeStatus = AVERROR_EOF");
+//            }else
+//            {
+//                avcodec_flush_buffers(_codecCtx);
+//                NSLog(@"receiveDecodeStatus = %d",receiveDecodeStatus);
+//            }
+//         
 //
-//        NSLog(@"AVERROR(EAGAIN) =  %d",AVERROR(EAGAIN));  35
-//        NSLog(@"AVERROR_EOF =  %d",AVERROR_EOF);          541478725
-//        NSLog(@"AVERROR(EINVAL) =  %d",AVERROR(EINVAL));  22
-        
-        
-        _packet.data = (uint8_t *)VideoData.bytes;
-        _packet.size = (int)VideoData.length;
-
-        int sendDecodeStatu,receiveDecodeStatus;
-    if (i <= 1000000){
-        sendDecodeStatu = avcodec_send_packet(_codecCtx, &_packet);
-        if (sendDecodeStatu == 0)
-        {
-            NSLog(@"sendPacket success");
-            receiveDecodeStatus = avcodec_receive_frame(self.codecCtx, self.frame);
-            if (receiveDecodeStatus == 0)
-            {
-                NSLog(@"receive Packet success");
-                [self dispatchAVFrame:self.frame];
-                
-            }else if(receiveDecodeStatus == AVERROR_EOF){
-                avcodec_flush_buffers(_codecCtx);
-                NSLog(@"receiveDecodeStatus = AVERROR_EOF");
-            }else
-            {
-                avcodec_flush_buffers(_codecCtx);
-                NSLog(@"receiveDecodeStatus = %d",receiveDecodeStatus);
-            }
-         
-
-        }else
-        {
-            NSLog(@"sendDecodeStatu = %d",sendDecodeStatu);
-        }
-        av_packet_unref(&_packet);
-        av_frame_unref(_frame);
-        }
+//        }else
+//        {
+//            NSLog(@"sendDecodeStatu = %d",sendDecodeStatu);
+//        }
+//        av_packet_unref(&_packet);
+////        av_frame_unref(_frame);
+//        }
 //    }
 }
 
@@ -164,6 +173,7 @@ static int count = 0;
         self.frame = NULL;
     }
     av_packet_unref(&_packet);
+    avformat_network_deinit();
 }
 
 
@@ -174,134 +184,54 @@ static int count = 0;
     }
     [_dstDataLock lock];
     
-    CVReturn theError;
-//    if (!self.pixelBufferPool){
-//        NSLog(@"!self.pixelBufferPool");
-//        NSMutableDictionary* attributes = [NSMutableDictionary dictionary];
-//        [attributes setObject:[NSNumber numberWithInt:kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange] forKey:(NSString*)kCVPixelBufferPixelFormatTypeKey];
-//        [attributes setObject:[NSNumber numberWithInt:frame->width] forKey: (NSString*)kCVPixelBufferWidthKey];
-//        [attributes setObject:[NSNumber numberWithInt:frame->height] forKey: (NSString*)kCVPixelBufferHeightKey];
-//        [attributes setObject:@(frame->linesize[0]) forKey:(NSString*)kCVPixelBufferBytesPerRowAlignmentKey];
-//        [attributes setObject:[NSDictionary dictionary] forKey:(NSString*)kCVPixelBufferIOSurfacePropertiesKey];
-//        theError = CVPixelBufferPoolCreate(kCFAllocatorDefault, NULL, (__bridge CFDictionaryRef) attributes, &_pixelBufferPool);
-//        if (theError != kCVReturnSuccess){
-//            NSLog(@"CVPixelBufferPoolCreate Failed");
-//        }
-//    }
     
+    NSLog(@"errorCode = %d",frame -> decode_error_flags);
+    
+    
+    CVReturn theError;
+
     CVPixelBufferRef pixelBuffer = nil;
-    //theError = CVPixelBufferPoolCreatePixelBuffer(NULL, self.pixelBufferPool, &pixelBuffer);
     
     theError = CVPixelBufferCreate(NULL, frame->width, frame->height, kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange, NULL, &pixelBuffer);
     if(theError != kCVReturnSuccess){
         NSLog(@"CVPixelBufferPoolCreatePixelBuffer Failed  theError = %d",theError);
     }
-//    CVPixelBufferRelease(pixelBuffer);
-//    CVPixelBufferPoolRelease(self.pixelBufferPool);
-//    return ;
-    
+
     CVPixelBufferLockBaseAddress(pixelBuffer, 0);
     size_t bytePerRowY = CVPixelBufferGetBytesPerRowOfPlane(pixelBuffer, 0);
     size_t bytesPerRowUV = CVPixelBufferGetBytesPerRowOfPlane(pixelBuffer, 1);
     void* base = CVPixelBufferGetBaseAddressOfPlane(pixelBuffer, 0);
     memcpy(base, frame->data[0], bytePerRowY * frame->height);
     base = CVPixelBufferGetBaseAddressOfPlane(pixelBuffer, 1);
-    //memcpy(base, frame->data[1], bytesPerRowUV * frame->height/2);
-    //CVPixelBufferUnlockBaseAddress(pixelBuffer, 0);
     uint32_t size = frame->linesize[1] * frame->height;
-//    printf("data[0] size is  %d\n", frame->linesize[0]);
-//    printf("data[1] size is  %d\n", frame->linesize[1]);
-//    printf("data[2] size is  %d\n", frame->linesize[2]);
-    
-   
-    
-    
-    uint8_t* dstData = malloc(2 * size);
-    if(dstData == NULL){
+
+    uint8_t * _dstData = malloc(size*2);
+    if(_dstData == NULL){
         NSLog(@"dstData = NUll");
         [_dstDataLock unlock];
         CVPixelBufferRelease(pixelBuffer);
         return;
     }
 
+    
+    
     for (int i = 0; i < 2 * size; i++){
         if (i % 2 == 0){
-            dstData[i] = frame->data[1][i/2];
+            _dstData[i] = frame->data[1][i/2];
         }else{
-//            if (count == 241) {
-//                return;
-//                if (i>470000) {
-//                    printf("i = %d %02x\n",i, frame->data[2][i/2]);
-//                }
-//                
-//            }
-            if(count == 241){
-                return;
-            }
-            dstData[i] = frame->data[2][i/2];
+
+            _dstData[i] = frame->data[2][i/2];
         }
     }
-    memcpy(base, dstData, bytesPerRowUV * frame->height/2);
+    //memset(frame->data[2], 0, size);
+    memcpy(base, _dstData, bytesPerRowUV * frame->height/2);
 //    int ret = CVPixelBufferCreateWithBytes(NULL, frame->width, frame->height, kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange, buffer, frame->linesize[0], NULL, 0, NULL, &pixelBuffer)
     
- //   free(dstData);
     NSLog(@"_playerLayer.pixelBuffer = pixelBuffer;");
     _playerLayer.pixelBuffer = pixelBuffer;
     CVPixelBufferRelease(pixelBuffer);
-    free(dstData);
-    
+    free(_dstData);
     [_dstDataLock unlock];
-}
-
-
-
-- (BOOL)initFFmpegDecoderimageView:(UIImageView *)imageView {
-    
-    self.imageView = imageView;
-    _playerLayer = [[AAPLEAGLLayer alloc]initWithFrame:imageView.bounds];
-    [self.imageView.layer addSublayer:_playerLayer];
-
-
-    /*注册所有的编码器，解析器，码流过滤器，只需要初始化一次*/
-    static dispatch_once_t once;
-    dispatch_once(&once, ^{
-        avcodec_register_all();
-    });
-    /*查找指定格式的解析器，这里我们使用H264*/
-    
-    
-    AVCodec *pCodec = avcodec_find_decoder(AV_CODEC_ID_H264);
-    if (pCodec == NULL) {
-        NSLog(@"codec not found");
-        return NO;
-    }
-    /*初始化解析器容器*/
-    if (_codecCtx == NULL) {
-        _codecCtx = avcodec_alloc_context3(pCodec);
-        if (_codecCtx == NULL) {
-            NSLog(@"Allocate codec context failed");
-            return NO;
-        }
-        av_dict_set(_codecCtx->priv_data, "tune", "zerolatency", 0);
-    }
-    /*打开指定的解析器*/
-    int ret = avcodec_open2(_codecCtx, pCodec, NULL);
-    if (ret != 0) {
-        NSLog(@"open codec error :%d", ret);
-        return NO;
-    }
-    /*AVFrame用来描述原始的解码音频和视频数据*/
-    if (_frame == NULL) {
-        _frame = av_frame_alloc();
-        if (_frame == NULL) {
-            NSLog(@"av_frame_alloc failed");
-            return NO;
-        }
-    }
-    
-//    avpicture_free(&avPicture);
-//    avpicture_alloc(&avPicture, AV_PIX_FMT_RGB24, _outputSize.width, _outputSize.height);
-    return YES;
 }
 
 
--- FFempegDemo/ViewController.m
+++ FFempegDemo/ViewController.m
@@ -12,10 +12,6 @@
 
 
 
-#define INBUF_SIZE 4096
-#define AUDIO_INBUF_SIZE 20480
-#define AUDIO_REFILL_THRESH 4096
-
 @interface ViewController ()
 
 @property (nonatomic, strong) UIImageView *imageView;
@@ -25,6 +21,7 @@
 @implementation ViewController
 {
     int _count;
+    BOOL _isRuning;
 }
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -46,14 +43,10 @@
 
     FFmpegDecode * ffmpegDecode = [[FFmpegDecode alloc]init];
     [ffmpegDecode initH264DecoderWithWidth:1280 height:720 imageView:self.imageView];
-//    [ffmpegDecode initFFmpegDecoderimageView:self.imageView];
     self.ffmpegDecode = ffmpegDecode;
     
     
-    
-    
-    
-    
+
 }
 
 //- (void)clickBtn
@@ -81,178 +74,37 @@
 
 - (void)clickBtn
 {
-    
-    for (int i = 240; i<1383; i++) {
+//    dispatch_queue_t queue = dispatch_queue_create("FFmpeg", 0);
+//    dispatch_async(queue, ^{
         
-        NSString * path = [[NSBundle mainBundle]pathForResource:[NSString stringWithFormat:@"bb%04d.h264",i] ofType:nil];
-        if (!path) {
-            NSLog(@"path 不存在");
-            i = 240;
-            continue;
+        for (int i = 1; i<1383; i++) {
+            
+            NSString * path = [[NSBundle mainBundle]pathForResource:[NSString stringWithFormat:@"bb%04d.h264",i] ofType:nil];
+            if (!path) {
+                NSLog(@"path 不存在");
+                i = 1;
+                continue;
+            }
+            NSLog(@"path = %@",path);
+            NSData * data = [NSData dataWithContentsOfFile:path];
+            
+            [self.ffmpegDecode H264decoderWithVideoData:data];
+            usleep(1000*10);
+            data = nil;
+            path = nil;
+            
+  
         }
-        NSLog(@"path = %@",path);
-        NSData * data = [NSData dataWithContentsOfFile:path];
 
-        
-//        if (data.length == 0) {
-//            NSLog(@"_packet.size == 0");
-//            continue;
-//        }
-        
-        [self.ffmpegDecode H264decoderWithVideoData:data];
-//        [self.ffmpegDecode releaseH264Decoder];
-        usleep(1000*20);
-        data = nil;
-        path = nil;
-    }
+//    });
+
+    
     
     
 }
 
-- (UIImage *)imageFromAVPicture:(AVPicture)pict width:(int)width height:(int)height
-{
-    
-    
-    CGBitmapInfo bitmapInfo = kCGBitmapByteOrderDefault;
-    CFDataRef data = CFDataCreateWithBytesNoCopy(kCFAllocatorDefault, pict.data[0], pict.linesize[0]*height,kCFAllocatorNull);
-    
-    CGDataProviderRef provider = CGDataProviderCreateWithCFData(data);
-    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-    CGImageRef cgImage = CGImageCreate(width,
-                                       height,
-                                       8,
-                                       24,
-                                       pict.linesize[0],
-                                       colorSpace,
-                                       bitmapInfo,
-                                       provider,
-                                       NULL,
-                                       NO,
-                                       kCGRenderingIntentDefault);
-    CGColorSpaceRelease(colorSpace);
-    UIImage *image = [UIImage imageWithCGImage:cgImage];
-    
-    CGImageRelease(cgImage);
-    CGDataProviderRelease(provider);
-    CFRelease(data);
-    
-    return image;
-}
 
 
-/*
- * Video decoding example
- */
-/*
-static void pgm_save(unsigned char *buf, int wrap, int xsize, int ysize,
-                     char *filename)
-{
-    FILE *f;
-    int i;
-    f = fopen(filename,"w");
-    fprintf(f, "P5\n%d %d\n%d\n", xsize, ysize, 255);
-    for (i = 0; i < ysize; i++)
-        fwrite(buf + i * wrap, 1, xsize, f);
-    fclose(f);
-}
-static int decode_write_frame(const char *outfilename, AVCodecContext *avctx,
-                              AVFrame *frame, int *frame_count, AVPacket *pkt, int last)
-{
-    int len, got_frame;
-    char buf[1024];
-    len = avcodec_decode_video2(avctx, frame, &got_frame, pkt);
-    if (len < 0) {
-        fprintf(stderr, "Error while decoding frame %d\n", *frame_count);
-        return len;
-    }
-    if (got_frame) {
-        printf("Saving %sframe %3d\n", last ? "last " : "", *frame_count);
-        fflush(stdout);
-        // the picture is allocated by the decoder, no need to free it
-        snprintf(buf, sizeof(buf), outfilename, *frame_count);
-        pgm_save(frame->data[0], frame->linesize[0],
-                 frame->width, frame->height, buf);
-        (*frame_count)++;
-    }
-    if (pkt->data) {
-        pkt->size -= len;
-        pkt->data += len;
-    }
-    return 0;
-}
-static void video_decode_example(const char *outfilename, const char *filename)
-{
-//    av_register_all();
-    avcodec_register_all();
-//    avformat_network_init();
-    
-    AVCodec *codec;
-    AVCodecContext *c= NULL;
-    int frame_count;
-    FILE *f;
-    AVFrame *frame;
-    uint8_t inbuf[INBUF_SIZE + AV_INPUT_BUFFER_PADDING_SIZE];
-    AVPacket avpkt;
-    av_init_packet(&avpkt);
-    // set end of buffer to 0 (this ensures that no overreading happens for damaged MPEG streams)
-    memset(inbuf + INBUF_SIZE, 0, AV_INPUT_BUFFER_PADDING_SIZE);
-    printf("Decode video file %s to %s\n", filename, outfilename);
-    // find the MPEG-1 video decoder
-    codec = avcodec_find_decoder(AV_CODEC_ID_H264);
-    if (!codec) {
-        fprintf(stderr, "Codec not found\n");
-        exit(1);
-    }
-    c = avcodec_alloc_context3(codec);
-    if (!c) {
-        fprintf(stderr, "Could not allocate video codec context\n");
-        exit(1);
-    }
-    if (codec->capabilities & AV_CODEC_CAP_TRUNCATED)
-        c->flags |= AV_CODEC_FLAG_TRUNCATED;
-//      we do not send complete frames
-//      For some codecs, such as msmpeg4 and mpeg4, width and height
-//      MUST be initialized there because this information is not
-//      available in the bitstream.
-//      open it
-    if (avcodec_open2(c, codec, NULL) < 0) {
-        fprintf(stderr, "Could not open codec\n");
-        exit(1);
-    }
-    f = fopen(filename, "rb");
-    if (!f) {
-        fprintf(stderr, "Could not open %s\n", filename);
-        exit(1);
-    }
-    frame = av_frame_alloc();
-    if (!frame) {
-        fprintf(stderr, "Could not allocate video frame\n");
-        exit(1);
-    }
-    frame_count = 0;
-    for (;;) {
-        avpkt.size = fread(inbuf, 1, INBUF_SIZE, f);
-        if (avpkt.size == 0)
-            break;
- //        NOTE1: some codecs are stream based (mpegvideo, mpegaudio) and this is the only method to use the because you cannot know the compressed data size before analysing it. BUT some other codec (msmpeg4, mpeg4) are inherently frame based, so you must call them with all the data for one frame exactly. You must also initialize 'width' and 'height' before initializing them.
- //        NOTE2: some codecs allow the raw parameters (frame size, sample rate) to be changed at any frame. We handle this, so you should also take care of it here, we use a stream based decoder (mpeg1video), so we feed decoder and see if it could decode a frame
-        avpkt.data = inbuf;
-        while (avpkt.size > 0)
-            if (decode_write_frame(outfilename, c, frame, &frame_count, &avpkt, 0) < 0)
-                exit(1);
-    }
-    // Some codecs, such as MPEG, transmit the I- and P-frame with a latency of one frame. You must do the following to have a chance to get the last frame of the video.
-    avpkt.data = NULL;
-    avpkt.size = 0;
-    decode_write_frame(outfilename, c, frame, &frame_count, &avpkt, 1);
-    fclose(f);
-    avcodec_close(c);
-    av_free(c);
-    av_frame_free(&frame);
-    printf("\n");
-}
-
-*/
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
